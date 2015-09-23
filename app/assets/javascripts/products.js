a9bfa7ae5296e01f6b3e76dfa185cc82ea0046d1
Cobi Moos <cobimoos100@gmail.com>
2015-09-23 17:01:47 -0400

    Ajax Form Integration Exercise Complete

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,30 @@
+$(document).on('ready page:load', function() {
+  $('#search-form').submit(function(event) {
+    event.preventDefault();
+    var searchValue = $('#search').val();
+
+  //   $.ajax({
+  //     url: '/products?search=' + searchValue,
+  //     type: 'GET',
+  //     dataType: 'html'
+  //   }).done(function(data){
+  //     $('#products').html.(data);
+  //   });
+  // });
+  // $.get('/products?search=' + searchValue)
+  //   .done(function(data){
+  //     console.log(data);
+  //     $('#products').html(data);
+  //   });
+   $.getScript('/products?search=' + searchValue);
+  });
+
+  $(window).scroll(function() {
+     var url = $('.pagination span.next').children().attr('href');
+     if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+      $('.pagination').text("Fetching more products...");list
+      console.log($('.pagination span.next').children().attr('href'));
+       $.getScript(url);
+     }
+   });
+});
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    if current_user
+      redirect_to products_url
+    else
+      @user = User.new
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to products_url, notice: "Signed up!"
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
